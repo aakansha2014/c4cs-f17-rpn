fbb93e4984cff7df5012a2b7c0676a8900252ea2
Akansha <aakansha2014@gmail.com>
2017-10-27 12:19:51 -0400

    Implemented ADD (+)

--- rpn.py
+++ rpn.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3 #
 
 def calculate(myarg):
-	pass
+	stack = list()
+	for token in myarg.split():
+		if token == '+':	
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1 + arg2
+			stack.append(result)
+		else:
+			stack.append(token)
+	print(stack)
 
-def main()
+def main():
 	while True:
 		calculate(input("rpm calc> "))
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 	main()
